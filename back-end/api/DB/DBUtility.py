141a25a24c0bd9547b4b9b68ac6873eaac753eff
Dev1 - rossi <94alex.rossi@gmail.com>
2022-07-11 15:16:17 +0200

    added-DbUtility

---
+++ back-end/api/DB/DBUtility.py
@@ -0,0 +1,24 @@
+import mysql.connector
+import json
+
+class DBUtility: 
+    
+    @staticmethod
+    def getLocalConnection():
+        print("creo la connessione")
+        with open("CloudMark/back-end/api/DB/DbCredential.json") as f:
+         db = json.load(f) 
+         connessione=None      
+        try:
+             # Connessione a MySQL
+            connessione = mysql.connector.connect(
+             # Params
+            host = db['endpoint'],
+            user = db['user'],
+            password = db['password'],
+            database = db['database'])
+            print("connessione eseguita correttamente")
+        except mysql.connector.Error as e:
+            print("Error reading data from MySQL table", e)
+        return connessione
+    
---
+++ back-end/api/DB/TestConnection.py
@@ -0,0 +1,7 @@
+from DBUtility import DBUtility
+class TestConnection:
+   def main():
+      DBUtility.getLocalConnection()
+  
+if __name__ == "__main__":
+    TestConnection.main()
