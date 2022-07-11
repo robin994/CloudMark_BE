1d3d7f054337f6ecc1fd2fd9b41619d0f2dd770e
Roberto <robin994@hotmail.it>
2022-07-11 16:41:13 +0200

    merge

--- back-end/api/DB/DBUtility.py
+++ back-end/api/DB/DBUtility.py
@@ -6,17 +6,11 @@ class DBUtility:
 
     @staticmethod
     def getLocalConnection():
-<<<<<<< HEAD
-        print("creo la connessione")
-        with open("CloudMark/back-end/API/DB/DbCredential.json") as f:
-         db = json.load(f) 
-         connessione=None      
-=======
+
         logging.info("creo la connessione")
         with open('./back-end/API/DB/DbCredential.json') as f:
          db = json.load(f)
          connessione=None
->>>>>>> fc846ddc01a797c290efe0fd7421403628f1026f
         try:
              # Connessione a MySQL
             connessione = mysql.connector.connect(
