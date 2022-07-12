50d1660b3cf0fe7908ef3b1454b01a9460196523
Roberto <robin994@hotmail.it>
2022-07-12 15:29:16 +0200

    fix

--- back-end/api/Dao/PresencetDao.py
+++ back-end/api/Dao/PresencetDao.py
@@ -1,19 +1,13 @@
 from Model.PresenceModel import PresenceModel
 from DB.DBUtility import DBUtility
+import logging
 
 def getPresence():
     conn = DBUtility.getLocalConnection()
     cursor = conn.cursor()
     cursor.execute("select * from presenza")
-    data = cursor.fetchone()
-    logging.info("Connection extablished to: ", data)
-    conn.close()
-    if(data == None):
-        print("this database doesn't have any data")
-        return("error")
-    else:
-        return data
-
+    return cursor.fetchone()
+    
 def addPresemce(presence: PresenceModel):
     conn = DBUtility.getLocalConnection()
     cursor = conn.cursor()
