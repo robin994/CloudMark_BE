f0256e290db8e4d6e2ba3ffa815a8193db232313
Dev1 - rossi <94alex.rossi@gmail.com>
2022-07-12 12:31:56 +0200

    added- __initi__.py, fixed-import-modules

--- back-end/api/Dao/Test/TestTipoAccount.py
+++ back-end/api/Dao/Test/TestTipoAccount.py
@@ -1,25 +1,18 @@
-import imp
 import logging
-import importlib.util
-import sys
-sys.path.insert(0,'CloudMark/back-end/api/Dao')
-from ...Dao.TipoAccountDao import TipoAccountDao
-
-class ServiceAlex:
+import sys,os
+sys.path.append(os.getcwd()+"/CLOUDMARK/back-end/api/Dao")
+sys.path.append(os.getcwd()+"/CLOUDMARK/back-end/api")
+from TipoAccountDao import TipoAccountDao
+class TestTipoAccount:
    def main():
-    print(TipoAccountDao.getAllTipoAccount())
-    # print(TipoAccountDao.getTipoAccountByNomeTipoAccount("administrator"))
+    # print(TipoAccountDao.getAllTipoAccount())
+    print(TipoAccountDao.getTipoAccountByNomeTipoAccount("administrator"))
     # ta = TipoAccount(nomeTipoAccount = "prova", funzioneProfilo = "scacco")
     # TipoAccountDao.insertTipoAccount(ta)
     # TipoAccountDao.deleteTipoAccount("prova")
-    # print(TipoPresenzaDao.getTipoPresenzabyNomeTipoPresenza("festivo"))
-    # tipoPresenza = TipoPresenza(nomeTipoPresenza="prova", percentualeMaggiorazione= 30, pagaOraria= 10)
-    # TipoPresenzaDao.insertTipoPresenza(tipoPresenza)
-    # TipoPresenzaDao.updateTipoAccount(tipoPresenza)
-    # TipoPresenzaDao.deleteTipoPresenza("prova")
     
     print
 if __name__ == "__main__":
-    ServiceAlex.main()
+    TestTipoAccount.main()
     
    
--- back-end/api/Dao/Test/TestTipoPresenza.py
+++ back-end/api/Dao/Test/TestTipoPresenza.py
@@ -1,2 +1,15 @@
-from Dao.TipoPresenzaDao import TipoPresenzaDao
-from ....api.Model.TipoPresenza import TipoPresenza
+import sys,os
+sys.path.append(os.getcwd()+"/CLOUDMARK/back-end/api/Dao")
+sys.path.append(os.getcwd()+"/CLOUDMARK/back-end/api")
+from TipoPresenzaDao import TipoPresenzaDao
+class TestTipoPresenza:
+  def main():
+      print(TipoPresenzaDao.getAllTipoPresenza())
+    # print(TipoPresenzaDao.getTipoPresenzabyNomeTipoPresenza("festivo"))
+    # tipoPresenza = TipoPresenza(nomeTipoPresenza="prova", percentualeMaggiorazione= 30, pagaOraria= 10)
+    # TipoPresenzaDao.insertTipoPresenza(tipoPresenza)
+    # TipoPresenzaDao.updateTipoAccount(tipoPresenza)
+    # TipoPresenzaDao.deleteTipoPresenza("prova")
+   
+if __name__ == "__main__":
+   TestTipoPresenza.main()
