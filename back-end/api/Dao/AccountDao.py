edd9f3449c3905d4dd1a5f674b2d57e5f8ecaf1c
Roberto <robin994@hotmail.it>
2022-07-12 15:50:31 +0200

    added features

--- back-end/api/Dao/AccountDao.py
+++ back-end/api/Dao/AccountDao.py
@@ -21,11 +21,32 @@ class AccountDao:
     def createAccount(account:AccountModel):
         connection = DBUtility.getLocalConnection()
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO cliente(id_account, user, password, abilitato, tipo_acount) VALUES(%s, %s, %s, %s, %s); COMMIT;", (
+        cursor.execute("INSERT INTO account(id_account, user, password, abilitato, tipo_acount) VALUES(%s, %s, %s, %s, %s); COMMIT;", (
                                 account['id'],
                                 account['user'],
                                 account['password'],
                                 account['abilitato'],
                                 account['tipo_acount']
+                        ))
+        return cursor.fetchall()
+
+    @staticmethod
+    def deleteAccountByID(id):
+        connection = DBUtility.getLocalConnection()
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM account WHERE id_account =" + id)
+        return cursor.commit()
+
+    @staticmethod
+    def updateAccountByID(account:AccountModel):
+        connection = DBUtility.getLocalConnection()
+        cursor = connection.cursor()
+        cursor.execute("UPDATE account SET user = '%s', password ='%s', abilitato = '%s', tipo_acount ='%s') where id_account = '%s'; COMMIT;", (
+                                account['user'],
+                                account['password'],
+                                account['abilitato'],
+                                account['tipo_acount'],
+                                account['id']
+
                         ))
         return cursor.fetchall()
