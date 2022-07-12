4729f6b0a94b95b24e815721f0288d30e2497e39
Dev1 - rossi <94alex.rossi@gmail.com>
2022-07-12 12:54:18 +0200

    Updated-Test-Classes

--- back-end/api/Dao/Test/TestTipoAccount.py
+++ back-end/api/Dao/Test/TestTipoAccount.py
@@ -2,11 +2,13 @@ import logging
 import sys,os
 sys.path.append(os.getcwd()+"/CLOUDMARK/back-end/api/Dao")
 sys.path.append(os.getcwd()+"/CLOUDMARK/back-end/api")
+sys.path.append(os.getcwd()+"/CLOUDMARK/back-end/api/Model")
 from TipoAccountDao import TipoAccountDao
+from Model.TipoAccount import TipoAccount
 class TestTipoAccount:
    def main():
     # print(TipoAccountDao.getAllTipoAccount())
-    print(TipoAccountDao.getTipoAccountByNomeTipoAccount("administrator"))
+    # print(TipoAccountDao.getTipoAccountByNomeTipoAccount("administrator"))
     # ta = TipoAccount(nomeTipoAccount = "prova", funzioneProfilo = "scacco")
     # TipoAccountDao.insertTipoAccount(ta)
     # TipoAccountDao.deleteTipoAccount("prova")
