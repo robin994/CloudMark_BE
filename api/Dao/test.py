cee942d3846fa520019bd6d8b7bf266c2796115f
Dev1 - rossi <94alex.rossi@gmail.com>
2022-07-19 15:42:37 +0200

    add-test-contract

--- api/Dao/test.py
+++ api/Dao/test.py
@@ -3,14 +3,17 @@ from unicodedata import name
 from uuid import uuid4
 from api.Dao.TestDao.TestAD import test_account_dao
 from api.Dao.TestDao.TestBD import test_business_dao
+from api.Dao.TestDao.TestCONTRACT import test_contract_dao
 
 
 class test:
   def main():
     
     logging_level = logging.WARNING
-    test_account_dao.main(logging_level)
-    #test_business_dao.main(logging_level)
+    # test_account_dao.main(logging_level)
+    # test_business_dao.main(logging_level)
+    test_contract_dao.main(logging_level)
+    
 
     # ###### TEST BusinessDao ######
     # in console mi aspetto 5 info, il primo ritorna il dizionario contenente tutti i record della tabella, il secondo contiene il dizionario con il record con ID=1, il terzo è il record da inserire in tabella, il quarto è il record che vado ad aggiornare e il quinto mi ritorna la conferma del DELETE del record con ID=2
