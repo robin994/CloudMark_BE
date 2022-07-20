a4345c9673b77fffe987212dc5cc82f5d0360ed7
Aldo <aldo.ravone@gmail.com>
2022-07-20 10:15:26 +0200

    ADD TestPresenceType

--- api/Dao/test.py
+++ api/Dao/test.py
@@ -7,19 +7,20 @@ from api.Dao.TestDao.TestCustomer import test_customer_dao
 from api.Dao.TestDao.TestOD import test_order_dao
 from api.Dao.TestDao.TestCONTRACT import test_contract_dao
 from api.Dao.TestDao.TestPRESENCE import test_presence_dao
-
+from api.Dao.TestDao.TestPresenceType import test_presence_type_dao
 
 
 class test:
   def main():
     
     logging_level = logging.WARNING
-    test_presence_dao.main(logging_level)
-    test_account_dao.main(logging_level)
-    test_business_dao.main(logging_level)
-    test_order_dao.main(logging_level)
-    test_contract_dao.main(logging_level)
-    test_customer_dao.main(logging_level)
+    # test_presence_dao.main(logging_level)
+    # test_account_dao.main(logging_level)
+    # test_business_dao.main(logging_level)
+    # test_order_dao.main(logging_level)
+    # test_contract_dao.main(logging_level)
+    # test_customer_dao.main(logging_level)
+    test_presence_type_dao.main(logging_level)
    
     # ########  TESTING tipoPresenzaDAO  ########## 
     #  # in console mi aspetto 5 info, il primo ritorna il dizionario contenente tutti i record della tabella, il secondo contiene il dizionario con il record con ID=1, il terzo è il record da inserire in tabella, il quarto è il record che vado ad aggiornare e il quinto mi ritorna la conferma del DELETE del record con ID=2   
