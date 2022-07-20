e3e45ca7de7ab831b3ab0657c9e8644e72e2dc17
Dev2 - Jacopo <jacopo_ba@outlook.it>
2022-07-20 10:27:13 +0200

    test accountType

---
+++ api/Dao/TestDao/TestAccountType.py
@@ -0,0 +1,55 @@
+import logging
+from Model.AccountType import AccountType
+from AccountTypeDao import AccountTypeDao
+
+class test_account_type_dao:
+    def main(*args):
+        logging.getLogger().setLevel(args[0])
+        ####### TEST BusinessDao ######
+        # in console mi aspetto 5 info, il primo ritorna il dizionario contenente tutti i record della tabella, il secondo contiene il dizionario con il record con ID=1, il terzo è il record da inserire in tabella, il quarto è il record che vado ad aggiornare e il quinto mi ritorna la conferma del DELETE del record con ID=2
+        total = 0
+        counter = 0
+        try:
+            total += 1
+            logging.info(AccountTypeDao.getAllAccountsType())
+            counter += 1
+        except(RuntimeError, TypeError, NameError)  as e:
+            logging.error("TypeAccountDao getAllAccountsType not passed")
+            logging.exception(e)
+        
+        accountTypeCreate = AccountType(id_account_type=3, accountTypeName='prova', function='prrr')
+        
+        try:
+            total += 1
+            logging.info(AccountTypeDao.createAccountType(accountTypeCreate))
+            counter += 1
+        except(RuntimeError, TypeError, NameError)  as e:
+            logging.error("TypeAccountDao createAccountType not passed")
+            logging.exception(e)
+
+        try:
+            total += 1
+            logging.info(AccountTypeDao.getAccountTypeById(accountTypeCreate.id_account_type))
+            counter += 1
+        except(RuntimeError, TypeError, NameError)  as e:
+            logging.error("TypeAccountDao getAccountTypeById not passed")
+            logging.exception(e)
+        
+        try:
+            total += 1
+            accountTypeToUpdate = AccountType(id_account_type=accountTypeCreate.id_account_type, accountTypeName='notProva', function='notPrr')
+            AccountTypeDao.updateAccountType(accountTypeToUpdate)
+            counter += 1
+        except(RuntimeError, TypeError, NameError)  as e:
+            logging.error("TypeAccountDao updateAccountType not passed")
+            logging.exception(e)
+        
+        try:
+            total += 1
+            AccountTypeDao.deleteAccountType(accountTypeCreate.id_account_type)
+            counter += 1
+        except(RuntimeError, TypeError, NameError)  as e:
+            logging.error("TypeAccountDao deleteAccountType not passed")
+            logging.exception(e)
+
+        logging.warning("Test TypeAccountDao, completati con successo %d / %d", counter, total)    
--- api/Dao/test.py
+++ api/Dao/test.py
@@ -2,6 +2,7 @@ import logging
 from unicodedata import name
 from uuid import uuid4
 from api.Dao.TestDao.TestAD import test_account_dao
+from api.Dao.TestDao.TestAccountType import test_account_type_dao
 from api.Dao.TestDao.TestBD import test_business_dao
 from api.Dao.TestDao.TestCustomer import test_customer_dao
 from api.Dao.TestDao.TestOD import test_order_dao
@@ -21,6 +22,7 @@ class test:
     test_contract_dao.main(logging_level)
     test_customer_dao.main(logging_level)
    
+    test_account_type_dao.main(logging_level)   
     # ########  TESTING tipoPresenzaDAO  ########## 
     #  # in console mi aspetto 5 info, il primo ritorna il dizionario contenente tutti i record della tabella, il secondo contiene il dizionario con il record con ID=1, il terzo è il record da inserire in tabella, il quarto è il record che vado ad aggiornare e il quinto mi ritorna la conferma del DELETE del record con ID=2   
     # logging.info(PresenceTypeDao.getAllPresenceType())
