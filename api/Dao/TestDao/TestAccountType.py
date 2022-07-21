8ff771a0acb52b932296bc2dea0ac1f07d995f37
Dev1 - Tesan <tesiu1234@gmail.com>
2022-07-21 14:25:25 +0200

    Edit: Editing test create update and delete

--- api/Dao/TestDao/TestAccountType.py
+++ api/Dao/TestDao/TestAccountType.py
@@ -9,13 +9,14 @@ class test_account_type_dao:
         # in console mi aspetto 5 info, il primo ritorna il dizionario contenente tutti i record della tabella, il secondo contiene il dizionario con il record con ID=1, il terzo è il record da inserire in tabella, il quarto è il record che vado ad aggiornare e il quinto mi ritorna la conferma del DELETE del record con ID=2
         total = 0
         counter = 0
-        try:
-            total += 1
-            logging.info(AccountTypeDao.getAllAccountsType())
-            counter += 1
-        except(RuntimeError, TypeError, NameError)  as e:
-            logging.error("TypeAccountDao getAllAccountsType not passed")
-            logging.exception(e)
+        
+        # try:
+        #     total += 1
+        #     logging.info(AccountTypeDao.getAllAccountsType())
+        #     counter += 1
+        # except(RuntimeError, TypeError, NameError)  as e:
+        #     logging.error("TypeAccountDao getAllAccountsType not passed")
+        #     logging.exception(e)
         
         accountTypeCreate = AccountType(id_account_type=3, accountTypeName='prova', function='prrr')
         
@@ -27,29 +28,29 @@ class test_account_type_dao:
             logging.error("TypeAccountDao createAccountType not passed")
             logging.exception(e)
 
-        try:
-            total += 1
-            logging.info(AccountTypeDao.getAccountTypeById(accountTypeCreate.id_account_type))
-            counter += 1
-        except(RuntimeError, TypeError, NameError)  as e:
-            logging.error("TypeAccountDao getAccountTypeById not passed")
-            logging.exception(e)
+        # try:
+        #     total += 1
+        #     logging.info(AccountTypeDao.getAccountTypeById(accountTypeCreate.id_account_type))
+        #     counter += 1
+        # except(RuntimeError, TypeError, NameError)  as e:
+        #     logging.error("TypeAccountDao getAccountTypeById not passed")
+        #     logging.exception(e)
         
-        try:
-            total += 1
-            accountTypeToUpdate = AccountType(id_account_type=accountTypeCreate.id_account_type, accountTypeName='notProva', function='notPrr')
-            AccountTypeDao.updateAccountType(accountTypeToUpdate)
-            counter += 1
-        except(RuntimeError, TypeError, NameError)  as e:
-            logging.error("TypeAccountDao updateAccountType not passed")
-            logging.exception(e)
+        # try:
+        #     total += 1
+        #     accountTypeToUpdate = AccountType(id_account_type=accountTypeCreate.id_account_type, accountTypeName='notProva', function='notPrr')
+        #     AccountTypeDao.updateAccountType(accountTypeToUpdate)
+        #     counter += 1
+        # except(RuntimeError, TypeError, NameError)  as e:
+        #     logging.error("TypeAccountDao updateAccountType not passed")
+        #     logging.exception(e)
         
-        try:
-            total += 1
-            AccountTypeDao.deleteAccountType(accountTypeCreate.id_account_type)
-            counter += 1
-        except(RuntimeError, TypeError, NameError)  as e:
-            logging.error("TypeAccountDao deleteAccountType not passed")
-            logging.exception(e)
+        # try:
+        #     total += 1
+        #     AccountTypeDao.deleteAccountType(accountTypeCreate.id_account_type)
+        #     counter += 1
+        # except(RuntimeError, TypeError, NameError)  as e:
+        #     logging.error("TypeAccountDao deleteAccountType not passed")
+        #     logging.exception(e)
 
         logging.warning("Test TypeAccountDao, completati con successo %d / %d", counter, total)    
