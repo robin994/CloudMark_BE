2a4fdb5263e121c61301bbe2a7ede43bd30a504c
Dev1 - Tesan <tesiu1234@gmail.com>
2022-07-21 14:22:37 +0200

    Edit: moved id_account_type to AccountType model

--- api/Model/AccountType.py
+++ api/Model/AccountType.py
@@ -2,7 +2,9 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class AccountType(BaseModel):
-    id_account_type: Optional[int]
+class NewAccountType(BaseModel):
     accountTypeName: Optional[str]
     function: Optional[str]
+
+class AccountType(NewAccountType):
+    id_account_type: str
