678b9e4a02a67ef2b77035daa58a0359242dce66
Roberto <robin994@hotmail.it>
2022-07-21 21:03:54 +0200

    fix

--- api/Dao/CallBackResponse.py
+++ api/Dao/CallBackResponse.py
@@ -7,12 +7,12 @@ class CallBackResponse:
         return CallBackResponse.genericResponse(res = response, lenght=response, error= None, description = description )
     
     @staticmethod
-    def error(error_message, description): 
-        return CallBackResponse.genericResponse(res= None, lenght=None, error= error_message, description = description )
+    def error(error_message): 
+        return CallBackResponse.genericResponse(res= None, lenght=None, error= "ERROR", description = error_message )
     
     @staticmethod
     def bad_request(error_message , description): 
-        return CallBackResponse.genericResponse(res= None, lenght=None, error= error_message, description = description )
+        return CallBackResponse.genericResponse(res= None, lenght=None, error= "BAD REQUEST", description = error_message )
     
     @staticmethod
     def genericResponse(res=None, lenght=0, error= None, description = None):
