6687b1af4da9c23164feea5498f81b1bd3a39c5e
Niskola <83880514+CamelTrue@users.noreply.github.com>
2022-07-22 11:17:16 +0200

    Edit: Change model

--- api/Model/PresenceType.py
+++ api/Model/PresenceType.py
@@ -1,10 +1,11 @@
 from typing import Optional
-
 from pydantic import BaseModel
 
 
-class PresenceType(BaseModel):
-    id_presence_type: Optional[int]
+class NewPresenceType(BaseModel):
     name: Optional[str]
     percentage_increase: Optional[int]
     hourly_pay: Optional[int]
+
+class PresenceType(NewPresenceType):
+    id_presence_type: str
