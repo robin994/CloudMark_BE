5e70bf94775f9a9eae7f841786a82de93f37b4f0
Roberto <robin994@hotmail.it>
2022-07-24 23:43:56 +0200

    fix

--- api/Api.py
+++ api/Api.py
@@ -254,7 +254,7 @@ async def get_presence_by_primary_key(id_presence, id_employee):
 async def get_all_presence():
     return PresenceDao.getAllPresence()
 
-@app.post("/presence/load_employee", tags=["Presence"])
+@app.get("/presence/load_employee={id_employee}", tags=["Presence"])
 async def get_presences_by_employee(id_employee:str):
     return PresenceDao.getPresencesByEmployee(id_employee)
 
