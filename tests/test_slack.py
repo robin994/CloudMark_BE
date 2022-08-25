cfbc55c6bab82d7a00bd0f6a771cfa3af300bac7
JJJJJJack <97jack@live.it>
2022-08-25 17:15:33 +0200

    FIX: deps conflicts, tox warnings for pytest execution

--- tests/test_slack.py
+++ tests/test_slack.py
@@ -1,7 +1,12 @@
 from mysql.connector.connection_cext import CMySQLConnection
+from fastapi.testclient import TestClient
 from DB import DBUtility
+from slack import app
 
 
 def test_DBUtility():
-    print(f"Type = {type(DBUtility.getLocalConnection())}")
     assert type(DBUtility.getLocalConnection()) is CMySQLConnection
+
+
+def test_app():
+    pass
